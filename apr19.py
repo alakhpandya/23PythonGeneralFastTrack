f06a22e8102848370ca04d0072b78257355c8295
alakhpandya <alakhpandya@gmail.com>
2023-04-19 13:07:05 +0530

    Create apr19.py

---
+++ apr19.py
@@ -0,0 +1,96 @@
+"""
+print("Good Morning!")
+print('How are you?')		# we can also use single quotes
+"""
+
+# Python print statement by default ends with \n
+
+'''
+Multi Line
+Comment
+'''
+
+"""
+Another
+way to do
+multiline
+comments
+"""
+
+# print following messages on your screen as they appear:
+"""
+I don't want to sell my car as it is working perfectly!
+He said to me "You should sell your car because it is so old!"
+
+When I refused, he said "Ok, but I'll still insist you to change your car before it starts demanding high maintenance."
+
+It's not at all a big deal to say someone "No, I can't do that for you."
+"""
+"""
+print('He said to me "You should sell your car because it is so old!"')
+print("Hello!", "How are you?")
+
+print('When I refused, he said "Ok, but',
+"I'll still insist you to change your car before it starts demanding high",
+'maintenance"')
+"""
+
+# 'end' statement
+# print("Name: Alakh", end = "")
+# print("Name: Alakh", end = " ")
+# print("Name: Alakh", end = "\t")
+# print("Name: Alakh", end = "\n")
+# print("Name: Alakh", end = "Python")
+# print("Name: Alakh", end = "\b\b\b")
+# print("Pandya")
+
+# print the following message:
+# Location of Python interpreter is: "C:\new folder\temp\Python\"
+
+print('Location of Python interpreter is: "C:\\new folder\\temp\\Python\\"')
+
+print("It's not at all a big deal to say someone \"No, I can't do that for you.\"")
+
+
+# Variables in Python
+"""
+a = 25
+print(a)
+print("a =", a)
+
+b = 17.7
+print("b =", b)
+
+print("Type of a:", type(a))
+print("Type of b:", type(b))
+
+c = 'myString'
+print("c =", c)
+print("Type of c:", type(c)) 
+"""
+
+# Taking inputs:
+print("Enter a:")
+a = input()
+print("a =", a)
+
+
+b = input("Enter b: ")
+print("b =", b)
+
+# print(int(a) + float(b))
+
+
+"""
+a = b
+ <--
+b = a
+"""
+a = int(a)
+b = float(b)
+
+print(type(a))
+print(type(b))
+
+print(a + b)
+# concat
