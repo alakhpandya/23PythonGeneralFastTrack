10b987924a5ba5a9f7d4e4a1b534efc12bde86d6
alakhpandya <alakhpandya@gmail.com>
2023-05-01 15:55:21 +0530

    apr30

---
+++ ap26.py
@@ -0,0 +1,44 @@
+"""
+1. Write a program to take a string from user and print its character lying at index no 5. Assume that the user will always input a string having more than 6 characters.
+
+Problem Constrains:
+len(A) > 6
+
+2. Write a program to take two strings from user and print both of them on seperate lines.
+
+3. Write a program to take two strings from user and print both with their names.
+
+Example input:
+Hello
+World
+
+Output:
+a = Hello
+b = World
+
+Solution:
+a = input("Enter input-1: ")
+b = input("Enter input-2: ")
+print("a =", a)
+print("b =", b)
+
+4. Write a program to take two strings from user and concat both and print.
+
+Example input:
+Hello
+World
+
+Output:
+HelloWorld
+"""
+# Boolean data type: True, False
+
+# print("Hello")
+# print(5 > 1)
+# print(5 > 10)
+
+a = True
+# a = "true"
+print(a)
+print(type(a))
+
---
+++ apr30.py
@@ -0,0 +1,30 @@
+# string slicing
+s1 = 	"Summer is at the peak and swimming pools are overcrowded at the weekends."
+# index: 0123456789...............................................................
+# -ve in:................................................................987654321
+"""
+print(s1[8])
+print(s1[-9])
+print(s1[-14])
+
+
+print(s1[4 : 20])
+print(s1[2 : 25])
+
+s2 = s1[5 : 15]	# slicing never changes the original data
+print(s1)
+print(s2)
+"""
+
+# print(s1[375])
+# print(s1[5 : 375])
+# print(s1[5 : -5])
+
+"""
+s2 = input("Enter a string: ")
+print(s2[3 : -4])
+"""
+# Write a program to take a string from user and remove first 4 characters & last 5 characters from it and print.
+# Write a program to take a string from user and remove first 2 characters & last 3 characters from it and print without using negative index.
+
+
