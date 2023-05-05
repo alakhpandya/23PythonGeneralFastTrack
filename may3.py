f11bc0d7f6109264671efb14d4191719bc43f60e
alakhpandya <alakhpandya@gmail.com>
2023-05-05 14:04:44 +0530

    may3

---
+++ may3.py
@@ -0,0 +1,9 @@
+# s = "Python is awesome"
+# a = len(s)
+# print(s[2 : a - 3])
+
+# print(s[2 : len(s) - 3])
+"""
+1. Write steps to solve the following problem on a paper - 
+Ask a string from user and also ask how many characters does she want to cut from the begining and from the end and print the resultant string.
+"""
