e3d0f0f71bae18ddbc898d7593a592664db93c49
alakhpandya <alakhpandya@gmail.com>
2023-05-19 16:08:29 +0530

    Create may19.py

---
+++ may19.py
@@ -0,0 +1,93 @@
+"""
+1. Arithmetic Operators: +, -, *, /, %, //, **
+2. Logical Operators: and, or, not
+3. Bitwise Operators: &, |, ~
+4. Assignment Operators: =, +=, -=, *=, /=, %=, //=, **=
+5. Comparision Operators/Relational Operators: >, <, ==, <=, >=, !=
+6. Identity Operators: is, is not
+7. Membership Operators: in, not in
+"""
+# x = 5
+# y = 2
+# print(x % y)
+# print(x ** y)
+
+# req_qualification = "MCA"
+# req_experience = 5
+# qualification = input("Qualification: ")
+# experience = int(input("Experience: "))
+# print(qualification == req_qualification or experience == req_experience)
+
+
+# username: uday
+# password: uday123
+
+# x = 3
+# y = 5
+# print(x <= y)
+"""
+# a = 5
+#  <--
+
+x = 7
+# x = x + 5   =>  x += 5
+y = 5
+# x = x + y   or  x += y
+x -= y  #   x = x - y   =   2
+x *= y  #   x = x * y   =   35
+x /= y  #   x = x / y   =   1.4
+x //= y #   x = x // y  =   1
+x **= y #   x = x ** y  =   16807
+x %= y  #   x = x % y   =   2
+"""
+"""
+myList = [1,2,3,4]
+yourList = [1,2,3,4]
+
+print(myList == yourList)
+print(myList is yourList)
+"""
+
+# myList = [1,2,3,4]
+# print(5 in myList)
+# print(3 in myList)
+# print(2.0 in myList)
+
+
+# Lists are mutable (Their methods change the original lists)
+"""
+myList = [1,2,3,4]
+myList.append(100)
+print(myList)
+"""
+# Tuples are immutable (Their methods do not change the original tuples)
+"""
+myTuple = (1, "myString", 2, 56.6, 3, 4, [11,22], (34, 43), 4)
+# myTuple.append(100)
+print(myTuple)
+print(myTuple.index(4, 3, 10))
+
+print(myTuple.count(4))
+"""
+
+# Sets: Unordered & Mutable collections of members
+s = {"Kit kat", "5 star", "Dairy Milk", "Munch", "Perk", "Milky Bar", "Fuse"}
+print(s)
+# print(s[4])
+# print(s[4 : 7])
+
+# Programming Related Methods
+# Mathematical Methods
+
+s.add("Snickers")
+# s.clear()
+# print(s)
+
+
+"""
+s1 = {1,2,3,4}
+s2 = {3,4,5,6}
+s3 = {5,6,7,8}
+"""
+
+# Next Class: How to create Empty Set
